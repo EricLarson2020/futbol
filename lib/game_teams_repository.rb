4af2adf6fa436e35796e86e3c5f50e09e6a0e7c1
Jane Greene <janegreenecasa@gmail.com>
2020-04-11 17:05:51 -0600

    Added methods and tests for most and fewest tackles to season repo class

--- lib/game_teams_repository.rb
+++ lib/game_teams_repository.rb
@@ -7,30 +7,30 @@ class GameTeamsRepository
     @games_collection = CsvHelper.generate_game_array(game_path)
     @game_teams_collection = CsvHelper.generate_game_teams_array(game_team_path)
   end
-
-  def most_tackles(season_id)
-    games_in_the_season = []
-    highest_tackles = 0
-    highest_team_id = 0
-    @games_collection.each do |game|
-      if game.season == season_id
-        games_in_the_season << game.game_id
-      end
-    end
-    # require "pry"; binding.pry
-    games_in_the_season.each do |season_game|
-      @game_teams_collection.each do |game_team|
-        if game_team.game_id == season_game
-          if game_team.tackles > highest_tackles
-            # require "pry"; binding.pry
-            highest_tackles = game_team.tackles
-            highest_team_id = game_team.team_id
-          end
-        end
-      end
-    end
-    # find_team_id(highest_team_id)
-    highest_team_id
-  end
+#MOVED TO SeasonRepository still may not need this class #######
+  # def most_tackles(season_id)
+  #   games_in_the_season = []
+  #   highest_tackles = 0
+  #   highest_team_id = 0
+  #   @games_collection.each do |game|
+  #     if game.season == season_id
+  #       games_in_the_season << game.game_id
+  #     end
+  #   end
+  #   # require "pry"; binding.pry
+  #   games_in_the_season.each do |season_game|
+  #     @game_teams_collection.each do |game_team|
+  #       if game_team.game_id == season_game
+  #         if game_team.tackles > highest_tackles
+  #           # require "pry"; binding.pry
+  #           highest_tackles = game_team.tackles
+  #           highest_team_id = game_team.team_id
+  #         end
+  #       end
+  #     end
+  #   end
+  #   # find_team_id(highest_team_id)
+  #   highest_team_id
+  # end
 
 end
